153d05b31d616943b2f6dd57098eccef1928118f
wholdenwingate <wiholdenwingate@davidson.edu>
2023-09-13 13:21:40 -0400

    CSS self portrait 1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 
 /* javascript */
-
+let elements = document.querySelectorAll("div");
+console.log(`This portrait contains ${elements.length} HTML div elements`);
 var bauhaus = `
 
 Staaatliches Bauhaus (1919-1933)
